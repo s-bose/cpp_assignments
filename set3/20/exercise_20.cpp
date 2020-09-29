fd23929b2ac35194dabec2382a7f926f3acddfb5
Shiladitya Basu <shiladitya_basu@live.com>
2020-09-29 18:04:06 +0530

    well

--- set3/20/exercise_20.cpp
+++ set3/20/exercise_20.cpp
@@ -12,11 +12,11 @@ int main()
 
         // convert the environment variables to std::string and store in an array
     std::string env_array[length];
-    for (int i = 0; *environ != nullptr; ++environ, ++i)
-        env_array[i] = std::string(*environ);
+    for (int idx = 0; *environ != nullptr; ++environ, ++idx)
+        env_array[idx] = std::string(*environ);
 
     quicksort(env_array, 0, length);
 
-    for (int i = 0; i < length; ++i)
-        std::cout << env_array[i] << '\n';
+    for (size_t idx = 0; idx < length; ++idx)
+        std::cout << env_array[idx] << '\n';
 }
